cff612fbf5142a843de7ed5e8e026d452e91830b
Peter <38251051+Boatwang@users.noreply.github.com>
2022-07-09 18:57:55 +0800

    Update CallbackServer.java

--- gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/common/CallbackServer.java
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/common/CallbackServer.java
@@ -44,14 +44,14 @@ public class CallbackServer extends HttpServlet {
         Map<String, String> respMap =null;
 
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessId = "LTAI5tJWAWV361sb2kLLXLsk";
-        String accessKey = "RpgO18Qd66T3t9wOsGPpjS80Zxi5SC";
+        String accessId = "";
+        String accessKey = "";
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-qingdao.aliyuncs.com";
         // 填写Bucket名称，例如examplebucket。
         String bucket = "gulimall-mrwang";
         // 填写Host地址，格式为https://bucketname.endpoint。
-        String host = "https://gulimall-mrwang.oss-cn-qingdao.aliyuncs.com";
+        String host = "";
         // 设置上传回调URL，即回调服务器地址，用于处理应用服务器与OSS之间的通信。OSS会在文件上传完成后，把文件上传信息通过此回调URL发送给应用服务器。
 //        String callbackUrl = "https://192.168.0.0:8888";
         // 设置上传到OSS文件的前缀，可置空此项。置空后，文件将上传至Bucket的根目录下。
